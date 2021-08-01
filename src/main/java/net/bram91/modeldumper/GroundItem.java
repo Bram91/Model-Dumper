c907fd652801dead192d1205b925f14d505196eb
Connor Younglund <cdy@airmail.cc>
2021-08-01 15:18:33 -0400

    remove unnecessary getter annotation

--- src/main/java/net/bram91/modeldumper/GroundItem.java
+++ src/main/java/net/bram91/modeldumper/GroundItem.java
@@ -2,7 +2,6 @@ package net.bram91.modeldumper;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import net.runelite.api.Model;
 import net.runelite.api.TileItem;
 import net.runelite.api.coords.WorldPoint;
@@ -10,9 +9,7 @@ import net.runelite.api.coords.WorldPoint;
 @Data
 @Builder
 class GroundItem {
-    @Getter
     private int id;
-    
     private TileItem item;
     private WorldPoint location;
     private int quantity;
